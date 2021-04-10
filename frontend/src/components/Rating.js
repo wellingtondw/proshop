31fe5992cd94834dc818d035e60cf5f6794331ae
wellington <wellingtonslpbi@hotmail.com>
2021-04-10 11:17:49 -0300

    refactoring rating

--- frontend/src/components/Rating.js
+++ frontend/src/components/Rating.js
@@ -1,72 +1,24 @@
 import PropTypes from 'prop-types'
 
+const arrayWithFiveElements = Array.from({ length: 5 }, (v, i) => i)
+
 const Rating = ({ value, text, color = '#f8e825' }) => {
   return (
     <div className='rating'>
-      <span>
-        <i 
-          style={{ color }}
-          className={
-            value >= 1 
-            ? 'fas fa-star' 
-            : value >= 0.5 
-            ? 'fas fa-star-half-alt' 
-            : 'far fa-star'
-          }
-        />
-      </span>
-
-      <span>
-        <i 
-          style={{ color }}
-          className={
-            value >= 2
-            ? 'fas fa-star' 
-            : value >= 1.5 
-            ? 'fas fa-star-half-alt' 
-            : 'far fa-star'
-          }
-        />
-      </span>
-
-      <span>
-        <i 
-          style={{ color }}
-          className={
-            value >= 3 
-            ? 'fas fa-star' 
-            : value >= 2.5 
-            ? 'fas fa-star-half-alt' 
-            : 'far fa-star'
-          }
-        />
-      </span>
-
-      <span>
-        <i 
-          style={{ color }}
-          className={
-            value >= 4 
-            ? 'fas fa-star' 
-            : value >= 3.5 
-            ? 'fas fa-star-half-alt' 
-            : 'far fa-star'
-          }
-        />
-      </span>
-
-      <span>
-        <i 
-          style={{ color }}
-          className={
-            value >= 5 
-            ? 'fas fa-star' 
-            : value >= 4.5 
-            ? 'fas fa-star-half-alt' 
-            : 'far fa-star'
-          }
-        />
-      </span>
+      {arrayWithFiveElements.map((_, index) => (
+        <span key={index}>
+          <i 
+            style={{ color }}
+            className={
+             value >= index + 1
+             ? 'fas fa-star' 
+             : value >= index + 1 - 0.5 
+             ? 'fas fa-star-half-alt' 
+             : 'far fa-star'
+            }
+          />
+        </span>
+      ))}
           
       {text && <p className='d-inline'>{text}</p>}
     </div>

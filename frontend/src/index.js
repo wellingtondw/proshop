0a8ee76bcff4cbf72ff82af892f11c1ff73262b0
wellington <wellingtonslpbi@hotmail.com>
2021-04-10 11:03:45 -0300

    create rating component

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/bootstrap.min.css'
-import './index.css';
+import './styles/index.css';
 import App from './App';
 
 ReactDOM.render(

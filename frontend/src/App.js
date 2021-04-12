0739f854bf2ddd44dcaaf0a39436d04e555305f7
wellington <wellingtonslpbi@hotmail.com>
2021-04-12 12:40:47 -0300

    config redux store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,9 @@
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
+import { Provider } from 'react-redux'
+import store from './redux/store'
+
 import Header from './components/Header'
 import Footer from './components/Footer'
 
@@ -9,16 +12,18 @@ import ProductDetails from './screens/ProductDetails'
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <main className='py-3'>
-        <Container>
-          <Route path='/' component={Home} exact />
-          <Route path='/product/:id' component={ProductDetails} exact />
-        </Container>
-      </main>
-      <Footer />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Header />
+        <main className='py-3'>
+          <Container>
+            <Route path='/' component={Home} exact />
+            <Route path='/product/:id' component={ProductDetails} exact />
+          </Container>
+        </main>
+        <Footer />
+      </Router>
+    </Provider>
   );
 }
 

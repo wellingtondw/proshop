c64685c96c81118e4635464a5e5b2137f7c60928
wellington <wellingtonslpbi@hotmail.com>
2021-04-13 11:02:11 -0300

    handle dispatch actions andd select in cart screen

--- frontend/src/screens/Cart.js
+++ frontend/src/screens/Cart.js
@@ -1,5 +1,29 @@
+import { useDispatch, useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
 
-const Cart = () => {
+import { addToCart } from '../redux/actions/cartActions'
+
+import { Row } from 'react-bootstrap'
+import Message from '../components/Message'
+import { useEffect } from 'react'
+
+
+
+const Cart = ({ match, location, history }) => {
+  const productId = match.params.id 
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+ 
+  const dispatch = useDispatch()
+
+  const cart = useSelector(state => state.cart)
+  const { cartItems } = cart
+
+  useEffect(() => {
+    if(productId) {
+      dispatch(addToCart({ id: productId, qty }))
+    }
+  }, [dispatch, productId, qty])
+
   return (
     <div>
       Cart

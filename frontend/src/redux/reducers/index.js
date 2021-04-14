f7141e469054088514a212d2fc5eaf75cf373bb4
wellington <wellingtonslpbi@hotmail.com>
2021-04-14 09:34:38 -0300

    create actions, constants and reducers login

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -1,11 +1,13 @@
 import { combineReducers } from 'redux'
 import { productListReducer, productDetailsReducer } from './productReducers'
 import { cartReducer } from './cartReducers'
+import { userReducer } from './userReducer'
 
 const reducers = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
-  cart: cartReducer
+  cart: cartReducer,
+  userLogin: userReducer
 })
 
 export default reducers
---
+++ frontend/src/redux/reducers/userReducer.js
@@ -0,0 +1,19 @@
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from '../constants/userConstants'
+
+const userInfoFromStorage = localStorage.getItem('userInfo')
+const initialState = userInfoFromStorage ? JSON.parse(userInfoFromStorage) : {}
+
+export const userReducer = (state = initialState, action) => {
+  switch(action.type) {
+    case USER_LOGIN_REQUEST: 
+      return { loading: true}
+    case USER_LOGIN_SUCCESS: 
+      return { loading: false, userInfo: action.payload }
+    case USER_LOGIN_FAIL: 
+      return { loading: false, error: action.payload }
+    case USER_LOGOUT:
+      return {}
+    default: 
+      return state
+  }
+}

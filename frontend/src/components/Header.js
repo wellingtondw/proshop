266b3638472c7c71fb0770618a29984533213567
wellington <wellingtonslpbi@hotmail.com>
2021-04-14 10:49:06 -0300

    show user in navbar and logout app

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,7 +1,18 @@
+import { useDispatch, useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import { NavDropdown, Container, Nav, Navbar } from 'react-bootstrap'
+import { logout } from '../redux/actions/userActions'
 
 const Header = () => {
+  const dispatch = useDispatch()
+
+  const userLogin = useSelector(state => state.userLogin)
+  const { userInfo } = userLogin
+
+  const logoutHandler = () => {
+    dispatch(logout())
+  }
+
   return (
     <header>
       <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
@@ -13,7 +24,16 @@ const Header = () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
               <Nav.Link as={Link} to="/cart"><i className='fas fa-shopping-cart'/> Cart</Nav.Link>
-              <Nav.Link as={Link} to="/login"><i className='fas fa-user'/> Sign In</Nav.Link>
+              {userInfo ? (
+                <NavDropdown title={userInfo.name} id='username'>
+                  <Link to='/profile'>
+                    <NavDropdown.Item as='span'>Profile</NavDropdown.Item>
+                  </Link>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
+                </NavDropdown>
+              ) : (
+                <Nav.Link as={Link} to="/login"><i className='fas fa-user'/> Sign In</Nav.Link>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
